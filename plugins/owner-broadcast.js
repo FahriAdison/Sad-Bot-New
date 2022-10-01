491fe96f4a92d603cebd4e62f536e55504514ce3
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-02 04:49:44 +0700

    Fix Broadcast

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -14,3 +14,23 @@ export default handler
 function readMore() {
 	return (String.fromCharCode(8206)).repeat(4001)
 }
+let handler = async (m, { conn, text, usedPrefix: _p }) => {
+	let wm = global.wm
+	let groups = Object.entries(await conn.groupFetchAllParticipating()).filter(([jid, chat]) => !chat?.announce).map(v => v[0]),
+		cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m,
+		teks = text ? text : cc.text
+	await m.reply(`_Mengirim pesan broadcast ke ${groups.length} group_`)
+	for (let id of groups) 
+	await conn.sendButton(id,'*—「 Broadcast 」—*\n' + teks,'Broadcast By Owner',[['⋮☰ Menu', _p + 'menu']], m, { viewOnce: true })
+	m.reply('Selesai Broadcast All Group')
+}
+handler.help = ['broadcastgroup']
+handler.command = /^(broadcast|bc)(group|grup|gc)$/i
+handler.tags = ['owner']
+handler.owner = true
+
+export default handler
+
+function readMore() {
+	return (String.fromCharCode(8206)).repeat(4001)
+}
