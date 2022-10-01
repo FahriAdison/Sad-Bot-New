5e6ff9693c312e115dae2b0f01427e700ad13152
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-01 22:00:19 +0700

    tqto

---
+++ plugins/info-tqto.js
@@ -0,0 +1,133 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix }) => {
+
+    let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+
+    //let wibu = `https://api.zacros.my.id/randomimg/loli`
+
+    let res = await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')
+
+    let txt = await res.text()
+
+    let arr = txt.split('\n')
+
+    let cita = arr[Math.floor(Math.random() * arr.length)]
+
+    let thumb = await(await fetch(cita)).buffer()
+
+    let who
+
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+
+    else who = m.sender
+
+    let user = global.db.data.users[who]
+
+let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
+
+*❉ Adiwajshing:*
+
+https://github.com/adiwajshing
+
+*❉ Nurutomo:*
+
+https://github.com/Nurutomo
+
+*❉ Istikmal:* 
+
+https://github.com/BochilGaming
+
+*❉ Ariffb:*
+
+https://github.com/Ariffb25
+
+*❉ Ilman:*
+
+https://github.com/ilmanhdyt
+
+*❉ Amirul:*
+
+https://github.com/amiruldev20
+
+*❉ Rasel:*
+
+https://github.com/raselcomel
+
+*❉ Fatur:*
+
+https://github.com/Ftwrr
+
+*❉ Rominaru:*
+
+https://github.com/Rominaru
+
+*❉ Kannachann:*
+
+https://github.com/Kannachann
+
+*❉ The.sad.boy01:*
+
+https://github.com/kangsad01
+
+*❉ Ameliascrf:*
+
+https://github.com/Ameliascrf
+
+*❉ Fokus ID:*
+
+https://github.com/Fokusdotid
+
+*❉ Johannes:*
+
+https://github.com/Johannes2803
+
+*❉ BrunoSobrino:*
+
+https://github.com/BrunoSobrino
+
+*❉ Krisna:*
+
+https://github.com/NevtBotz
+
+*❉ LitRHap:*
+
+https://github.com/LitRHap
+
+*❉ Rlxfly:*
+
+https://github.com/Rlxfly
+
+*❉ Aine:*
+
+https://github.com/Aiinne
+
+*▸ - - - —「 KANG RECODE 」— - - - ◂*
+
+*❉ Fahri:*
+
+https://github.com/FahriAdison
+
+*▸ - - - —「 SPESIAL HELPER 」— - - - ◂*
+
+*❉ Kannachan:*
+
+*❉ Johannes:*
+
+*❉ Krisna:*
+
+*❉ Rlxfly:*
+
+`
+
+conn.sendButton(m.chat, tqto,null,null,[['Menu',`${usedPrefix}m`]],m, { viewOnce: true})
+
+        }
+
+handler.help = ['tqto']
+
+handler.tags = ['main','info']
+
+handler.command = /^(credits|credit|thanks|thanksto|tqto)$/i
+
+export default handler
