e8efc1cd2cc573ccabd4abf71a496c2a92a18764
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-02 04:58:10 +0700

    Profile User

--- plugins/misc-profile.js
+++ plugins/misc-profile.js
@@ -1,14 +1,36 @@
 import PhoneNumber from 'awesome-phonenumber'
+import moment from 'moment-timezone'
 
 let handler = async (m, { conn }) => {
-	let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.sender,
-		username = await conn.getName(who),
-		about = (await conn.fetchStatus(who).catch(console.error) || {}).status || '-',
-		number = PhoneNumber('+' + who.split('@')[0]).getNumber('international'),
-		avatar = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-		url = API('violetics', '/api/canvas/whatsapp-profile', { username, about, number, avatar }, 'apikey')
-	conn.sendMessage(m.chat, { image: { url }}, { quoted: m })
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  var { status, setAt } = await conn.fetchStatus(who).catch(() => {
+          return {
+            status: "",
+            setAt: "",
+          };
+        });
+  let pp = await conn.profilePictureUrl((who), "image");
+   
+  try {
+    pp = await conn.ProfilePictureUrl(who)
+  } catch (e) {
+
+  } finally {
+    let about = (await conn.fetchStatus(who).catch(console.error) || {}).status || ''
+    let username = conn.getName(who)
+    let str = `
+• Name: ${username}, \n• Tag: @${who.replace(/@.+/, '')}, ${about ? '\n• Bio: ' + about : ''}, \n• Set At Bio: ${(setAt && moment(setAt).format("DD MMMM YYYY")) || "Unknown"}, \n• Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')},\n• Link: https://wa.me/${who.split`@`[0]}`.trim()
+
+    let mentionedJid = [who]
+    conn.sendFile(m.chat, pp, 'pp.jpeg', str, m, false, { contextInfo: { mentionedJid }})
+  }
 }
+handler.help = ['profile [@user]']
+handler.tags = ['tools']
 handler.command = /^(profile)$/i
+handler.group = false
 
 export default handler
+
+// parel dan kaliph
+// johnes dan cina
