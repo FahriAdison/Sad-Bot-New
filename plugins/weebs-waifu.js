3f45a30883f3e0da2196097a7ca80ea719a6e053
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-02 07:18:27 +0700

    Fix Waifu

--- plugins/weebs-waifu.js
+++ plugins/weebs-waifu.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 	let res = await fetch(`https://api.waifu.pics/sfw/${command}`)
 	if (!res.ok) throw await res.text()
 	let json = await res.json()
-	conn.sendButton(m.chat, `Random Image ${command.capitalize()}`, json.url, json.url, [['Next', usedPrefix + command]], m)
+	conn.sendButton(m.chat, `Random Image ${command.capitalize()}`, json.url, json.url, [['Next', usedPrefix + command]], m, { viewOnce: true })
 }
 handler.help = handler.alias = ['waifu', 'neko']
 handler.tags = ['weebs']
