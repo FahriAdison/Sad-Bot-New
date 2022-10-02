b4e5a3d7f81c52ec58ec055964a60148275363b2
Papah-Chan「xʏᴢ×ᴏғᴄ」 <92633211+FahriAdison@users.noreply.github.com>
2022-10-02 07:17:26 +0700

    Fix Command Ping

--- plugins/misc-speed.js
+++ plugins/misc-speed.js
@@ -79,10 +79,10 @@ _CPU Core(s) Usage (${cpus.length} Core CPU)_
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
 `, m)
 }
-handler.help = ['pingspeed']
+handler.help = ['ping','speed']
 handler.tags = ['info', 'tools']
 
-handler.command = /^(pingspeed)$/i
+handler.command = /^(ping|speed)$/i
 export default handler
 
 const more = String.fromCharCode(8206)
